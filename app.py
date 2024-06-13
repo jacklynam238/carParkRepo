4adaaf3361465b3214c9151fbbad71791319fac1
Jack Lynam <jack.lynam@ericsson.com>
2024-06-13 15:00:45 +0100

    test

--- app.py
+++ app.py
@@ -20,9 +20,25 @@ class Item(db.Model):
 def index():
     return render_template('index.html')
 
-@app.route('/booking', methods=['GET', 'POST'])
+@app.route('/booking')
 def booking():
     return render_template('booking.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route('/submit_booking', methods=['POST'])
 def submit_booking():
